277d9e1fd6748f35a2d48c8154ee2aae25fdf62d
woldy-tal <king@woldy.net>
2017-08-09 17:02:40 +0800

    modified:   src/Components/User/User.php 	modified:   src/views/xenon/common/bottom.blade.php

--- src/Components/User/User.php
+++ src/Components/User/User.php
@@ -118,6 +118,7 @@ class User{
             return false;
         }else{
           $userinfo=array_merge($userinfo,$third);
+
            if(isset($userinfo['is_admin']) && $userinfo['is_admin']==1){
              Session::put('admin',$userinfo);
            }
